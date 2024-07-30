ece4a25a4e00d85a14905fcc396a56b9496f07a5
Heba Yasser <hebay99@gmail.com>
2024-07-30 22:05:08 +0300

    add home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./components/landing/landing";
 import Login from "./components/login/login";
 import Register from "./components/login/register";
+// import Home from "./components/Home";
 
 function App() {
   return (
@@ -10,9 +11,13 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" Component={Landing} />
-          <Route path="/register" Component={Register}></Route>
-          <Route path="/login" Component={Login} />
-          <Route path="/Home" Component={Login} />
+          <Route path="/register" Component={Register}>
+            {/* <Route path="Home" Component={Home} /> */}
+          </Route>
+          <Route path="/login" Component={Login}>
+            {/* <Route path="Home" Component={Home} /> */}
+          </Route>
+          {/* <Route path="*" Component={NotFound} /> */}
         </Routes>
       </Router>
     </>
