7907e2fec68142d0643b942c1f1d200861740816
Heba Yasser <hebay99@gmail.com>
2024-08-01 19:05:55 +0300

    add home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -15,6 +15,80 @@ export default function Home() {
   const [isCompeleted, setICompeleted] = useState(true);
   const navigate = useNavigate();
 
+  // const toDos = async () => {
+  //   const user = await (
+  //     await fetch("http://localhost:3000/users?userName=heba")
+  //   ).json();
+  //   const userToDos = user[0].toDos;
+  //   return userToDos;
+  // };
+
+  // fetch("http://localhost:3000/users?userName=heba")
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error("Network response was not ok");
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     const toDos = data[0].toDos;
+  //     return toDos;
+  //   })
+  //   .catch((error) => {
+  //     console.error("There was a problem with the fetch operation:", error);
+  //   });
+  // console.log(toDos);
+
+  // const handleDelete = (todo) => {
+  //   fetch("http://localhost:3000/users", {
+  //     method: "PUT",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       toDos: toDos.filter((toDo) => toDo !== todo),
+  //     }),
+  //   });
+  // };
+
+  // Assuming you have a JSON server running on port 3000 with a 'todos' endpoint
+
+  // const fetchTodos = async () => {
+  //   try {
+  //     const response = await fetch("http://localhost:3000/users?userName=heba");
+  //     const todos = await response.json();
+  //     return todos[0].toDos;
+  //   } catch (error) {
+  //     console.error("Error fetching todos:", error);
+  //   }
+  // };
+
+  // fetchTodos()
+  //   .then((todos) => {
+  //     console.log(todos);
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error fetching todos:", error);
+  //   });
+
+  // const handleDelete = (todo: string) => {
+  //   const url = `http://localhost:3000/users?userName=heba`;
+
+  //   fetch(url, {
+  //     method: "DELETE",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error("Failed to delete todo");
+  //       }
+  //       console.log("Todo deleted successfully");
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error deleting todo:", error);
+  //     });
+  // };
+
   return (
     <>
       <div className="container-fluid body">
